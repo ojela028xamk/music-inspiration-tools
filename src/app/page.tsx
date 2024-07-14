f001a3827de450f1c7e6599bb47a1b4292798380
ojela028xamk <jerelans91@gmail.com>
2024-07-14 10:41:06 +0300

    react-use sass

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { Alert } from "@mui/material";
-import styles from "./page.module.css";
+import css from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={css.main}>
       <h1>Music Inspiration Tools</h1>
       <Alert severity="success">
         Here is a gentle confirmation that your action was successful.
